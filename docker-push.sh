0e99906f6ee130e47f4a22cbf50ecb3ebec5105b
Misagh Moayyed <mm1844@gmail.com>
2025-10-29 21:16:35 -0700

    update

--- docker-push.sh
+++ docker-push.sh
@@ -7,6 +7,6 @@ echo "$docker_psw" | docker login --username "$docker_user" --password-stdin
 
 image_tag=(`cat gradle.properties | grep "cas.version" | cut -d= -f2`)
 
-echo "Pushing CAS docker image tagged as v$image_tag to apereo/cas..."
-docker push apereo/cas:"v$image_tag" \
-	&& echo "Pushed apereo/cas:v$image_tag successfully.";
+echo "Pushing CAS docker image tagged as v$image_tag to mmoayyed/cas-oidc-docker..."
+docker push mmoayyed/cas-oidc-docker:"v$image_tag" \
+	&& echo "Pushed mmoayyed/cas-oidc-docker:v$image_tag successfully.";
