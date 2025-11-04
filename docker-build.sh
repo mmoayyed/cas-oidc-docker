e4c380c62707b56a72b2c451b4319ca33a5265e7
Misagh Moayyed <mm1844@gmail.com>
2025-11-03 20:09:32 -0800

    update

--- docker-build.sh
+++ docker-build.sh
@@ -5,6 +5,5 @@ image_tag=(`cat gradle.properties | grep "cas.version" | cut -d= -f2`)
 echo "Building CAS docker image tagged as [v$image_tag]"
 # read -p "Press [Enter] to continue..." any_key;
 
-docker build --tag="mmoayyed/cas-oidc-docker:v$image_tag" . \
-  && echo "Built CAS image successfully tagged as mmoayyed/cas-oidc-docker:v$image_tag" \
-  && docker images "mmoayyed/cas-oidc-docker:v$image_tag"
+docker buildx build --platform linux/amd64 --tag="mmoayyed/cas-oidc-docker:v$image_tag-amd64" --load . 
+docker buildx build --platform linux/arm64 --tag="mmoayyed/cas-oidc-docker:v$image_tag-arm64" --load . 
--- docker-push.sh
+++ docker-push.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 read -p "Docker username: " docker_user
-read -s -p "Docker password: " docker_psw
 
-echo "$docker_psw" | docker login --username "$docker_user" --password-stdin
+echo $DOCKER_REGISTRY_TOKEN | docker login --username "$docker_user" --password-stdin
 
 image_tag=(`cat gradle.properties | grep "cas.version" | cut -d= -f2`)
 
 echo "Pushing CAS docker image tagged as v$image_tag to mmoayyed/cas-oidc-docker..."
-docker push mmoayyed/cas-oidc-docker:"v$image_tag" \
-	&& echo "Pushed mmoayyed/cas-oidc-docker:v$image_tag successfully.";
+
+docker push mmoayyed/cas-oidc-docker:"v$image_tag-amd64" 
+docker push mmoayyed/cas-oidc-docker:"v$image_tag-arm64" 
+
+docker manifest create mmoayyed/cas-oidc-docker:v${image_tag} \
+  --amend mmoayyed/cas-oidc-docker:v${image_tag}-amd64 \
+  --amend mmoayyed/cas-oidc-docker:v${image_tag}-arm64
+
+docker manifest push mmoayyed/cas-oidc-docker:v${image_tag}
